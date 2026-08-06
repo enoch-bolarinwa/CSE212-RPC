e1734abb92a6dc70b832790818a7459a742c4d81
enoch-bolarinwa <oluwafemi.bolarinwa@gmail.com>
2026-08-06 21:02:59 +0100

    week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,33 +13,46 @@ public class Node
     {
         // TODO Start Problem 1
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+       if (value == Data)
+    {
+        return;
     }
+
+    if (value < Data)
+    {
+        // Insert to the left
+        if (Left is null)
+            Left = new Node(value);
+        else
+            Left.Insert(value);
+    }
+    else
+    {
+        // Insert to the right
+        if (Right is null)
+            Right = new Node(value);
+        else
+            Right.Insert(value);
+    }
+}
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
-    }
+   if (value == Data)
+        return true;
+
+    if (value < Data)
+        return Left is not null && Left.Contains(value);
+    else
+        return Right is not null && Right.Contains(value);
+}
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
-    }
+    int leftHeight = Left is null ? 0 : Left.GetHeight();
+    int rightHeight = Right is null ? 0 : Right.GetHeight();
+    return 1 + Math.Max(leftHeight, rightHeight);
+}
 }
